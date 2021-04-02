f74636c6c3ee0a71e2287f889967924f7e517d14
vanessakg <58560454+vanessakg@users.noreply.github.com>
2021-04-02 12:48:35 -0500

    server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
-app.use(express.static('./public'));
+app.set('public', path.join(__dirname, 'public'));
+app.set("view engine", "ejs")
 
 const db = mysql.createConnection({
     user: "DMV_S2021",
@@ -22,14 +24,15 @@ app.post("/AdminInfo", (req, res) => {
     const adminID = req.body.adminID;
     const adminPW = req.body.adminPW;
     db.query(
-        "SELECT * FROM Admin", [adminID, adminPW], 
+        "SELECT * FROM Admin", [adminID, adminPW],
         (err, result) => {
             if(err) throw err;
             console.log(result)
+            res.send(result)
             //console.log("Admin:", `${adminID}`)
             //console.log("Password: ", `${adminPW}`)
     })
-    console.log("Logging in...")
+    res.render('adminLogin')
 });
 
 app.get("/lostItemsAdmin", (req, res) => {
@@ -41,6 +44,11 @@ app.get("/lostItemsAdmin", (req, res) => {
             res.send(result)
         }
     )
+    
+})
+
+app.get("/", (req, res) => {
+    res.render("login")
 })
 
 app.listen('3001', () => { 
